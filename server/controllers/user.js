2757209db533a690df49fdef0fae1f0afb5d2433
Anthony Infortunio <contact@anthonyinfortun.io>
2023-03-31 12:32:29 -0400

    finish the user controller

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -2,7 +2,7 @@ const UserModel = require('../models/User');
 
 //create user
 let createUser = async function(firstName,lastName,age,email,password){
-  UserModel.create({
+  return await UserModel.create({
     firstName: firstName,//firstName
     lastName: lastName,//lastName
     age: age,//age
@@ -12,13 +12,29 @@ let createUser = async function(firstName,lastName,age,email,password){
     likes: [],//likes
     shares: [],//shares
     dateCreated: Date.now()//dateCreated
-  })
+  });
 };
 //get user
+let getUser = async function(docID){
+  return await UserModel.findById(docID);
+};
 //get all users
+let getAllUsers = async function(){
+  return await UserModel.find({});
+};
 //update user
+let updateUser = async function(docID,updatedUser){
+  return await UserModel.findByIdAndUpdate(docID,updatedUser);
+};
 //delete user
+let deleteUser = async function(docID){
+  return await UserModel.findByIdAndDelete(docID);
+};
 
 module.exports = {
-  createUser
+  createUser,
+  getUser,
+  getAllUsers,
+  updateUser,
+  deleteUser
 };
